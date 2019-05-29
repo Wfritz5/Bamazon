400494cee92d151a2a1a2472516c40b88465cdcf
Will <wfritz5@gmail.com>
2019-05-29 13:38:04 -0600

    worked on bamazonManager.js addnewproduct function

--- constructors/bamazonManager.js
+++ constructors/bamazonManager.js
@@ -68,5 +68,104 @@ function products(low) {
         console.log("+----+------------------------------------------+------------------+----------+-----+\n");
         listOptions();
     });
+}
 
+//add inventory
+let addToInventory = () => {
+    connection.query("SELECT item_id, product_name, stock_quantity FROM product", function(err, results) {
+        if(err) throw err;
+        inquirer.prompt([
+            {
+                name : "choice",
+                type: "list",
+                choices: function() {
+                    let choiceArray = [];
+                    for (let i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].product_name);
+                    }
+                    return choiceArray;
+                },
+                message : "Which item would you like to add more inventory to?"
+            },
+            {
+                name : "quantity",
+                type: "input",
+                message: "How many shall we order?"
+            }
+        ]).then(function(answer) {
+            let itemChosen;
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].product_name === answer.choice) {
+                    itemChosen = results[i];
+                }
+            }
+            let updateStockCount = parseInt(itemChosen.stock_quantity) + parseInt(answer.quantity);
+            connection.query(
+                "UPDATE prodct SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: updateStockCount
+                    },
+                    {
+                        item_id: itemChosen.item_id
+                    }
+                ],
+                function(error) {
+                    if (error) throw error;
+                    console.log("You have successfully added " + answer.quantity + "units to your inventory. You now have" + updateStockCount + ".");
+                    listOptions();
+                }
+            );
+        });
+    });
+}
+
+//adding a new product
+
+let addNewProduct = () => {
+    connection.query("SELECT department_name FROM departments", function(err, results) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+            name : "product_name",
+            type : "input",
+            message: "What is the new product you will be adding?"
+            },
+            {
+                name : "department_name",
+                type : "list",
+                choice : function() {
+                    let choiceArray = []
+                        for (let i=0; i<results.length; i++) {
+                            choiceArray.push(results[i].department_name);
+                        }
+                        return choiceArray;
+                },
+                message : "Which department does this belong to?"
+            },
+            {
+                name : "price",
+                type : "input",
+                message : "How much does this item cost? (Please enter number)",
+                validate : function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]) .then(function(answer) {
+            connection.query(
+                "INSERT INTO product (product_name, department_name, price, stock_quantity) VALUES ('" + answer.product_name + "', '" + answer.department_name + "', '" + answer.price + "', '" + answer.stock_quantity + "')",
+				function(error) { 
+                    if(error) {
+                        console.log(error);
+                        throw error;
+                    }
+                    console.log("You have added " + answer.stock_quantity + " units of " + answer.product_name + ".");
+                    listOptions();
+                }
+            );
+        });
+    });
 }
